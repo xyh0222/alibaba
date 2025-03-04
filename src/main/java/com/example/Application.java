95fdc0774c75e649438630baa3ae58fb30499a4f
xuyihan <330789007@qq.com>
2025-03-04 23:23:18 +0800

    init

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,9 +1,7 @@
 package com.example;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan("com.example.mapper")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
